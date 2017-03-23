78b64def012b0915dc55b88852575626ed8728e0
kmgeu <kmgeu123@gmail.com>
2017-03-23 17:08:38 +0900

    add SessionConfig

--- TourOfAll/src/main/java/net/bulldozer/tourofall/configuration/WebInitializer.java
+++ TourOfAll/src/main/java/net/bulldozer/tourofall/configuration/WebInitializer.java
@@ -1,5 +1,10 @@
 package net.bulldozer.tourofall.configuration;
 
+import javax.servlet.Filter;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class WebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer{
@@ -18,4 +23,15 @@ public class WebInitializer extends AbstractAnnotationConfigDispatcherServletIni
 		return new String[]{"/"};
 	}
 
+	@Override
+	protected Filter[] getServletFilters() {
+		return new Filter[] {new HiddenHttpMethodFilter()};
+	}
+
+	@Override
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		super.onStartup(servletContext);
+		servletContext.addListener(new SessionListener());
+	}
+	
 }
